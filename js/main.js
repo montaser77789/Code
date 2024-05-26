7c0e6f0cc697776a503e82c83a1a136c3512ad62
montaser77789 <montasergohar@gmail.com>
2024-05-26 19:10:00 +0300

    Update in Style

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let items = document.querySelectorAll(".item");
 
-
-
 for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", function () {
-        for (let j = 0; j < items.length; j++) {
-            items[j].classList.remove("active");
-        }
-        this.classList.add("active");
-    });
+  items[i].addEventListener("click", function () {
+    for (let j = 0; j < items.length; j++) {
+      items[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
 }
+$(document).on('click', function(event) {
+  var target = $(event.target);
+  if (!target.closest('#myModal .modal-content').length && $('#myModal').is(':visible')) {
+    $('#myModal').modal('hide');
+  }
+});
